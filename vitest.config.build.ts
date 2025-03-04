47b90e447a61348e4a8da4fa5e218a5d7fd911ca
Henrique Artur <contato@henriqueartur.com>
2025-03-03 21:39:31 -0300

    🔧 fix `vitest`

--- vitest.config.build.ts
+++ vitest.config.build.ts
@@ -1,9 +1,11 @@
+import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   resolve: {
     preserveSymlinks: true,
   },
+  plugins: [tsconfigPaths()],
   test: {
     include: ["test/**/*.spec.ts"],
     exclude: ["test/dist/**"],
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,12 @@
+import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   resolve: {
     preserveSymlinks: true,
   },
+  plugins: [tsconfigPaths()],
   test: {
-    globals: true,
     include: ["src/**/*.spec.ts"],
     exclude: ["test/**"],
   },
