706ff1eddfda5c04251da5ca0d7cefd33d525014
Henrique Artur <contato@henriqueartur.com>
2025-03-13 20:29:36 -0300

    ✨ adding enuns: `reverse`, `slice`, `some` & `sort`

--- packages/core/src/enum.ts
+++ packages/core/src/enum.ts
@@ -1,19 +1,14 @@
-const map = <T, U>(
-  list: T[],
-  callback: (value: T, index?: number, array?: readonly T[]) => U,
-): U[] => list.map(callback);
-
-const filter = <T, S extends T>(
-  list: T[],
-  predicate: (value: T, index?: number, array?: readonly T[]) => value is S,
-): S[] => list.filter(predicate);
-
 const concat = <T>(list: T[], ...items: (T | ConcatArray<T>)[]): T[] => list.concat(...items);
 
 const every = <T>(
   list: T[],
   predicate: (value: T, index?: number, array?: readonly T[]) => boolean,
 ): boolean => list.every(predicate);
+
+const filter = <T, S extends T>(
+  list: T[],
+  predicate: (value: T, index?: number, array?: readonly T[]) => value is S,
+): S[] => list.filter(predicate);
 
 const find = <T>(
   list: T[],
@@ -27,6 +22,11 @@ const includes = <T>(list: T[], searchElement: T, fromIndex?: number): boolean =
 
 const join = <T>(list: T[], separator?: string): string => list.join(separator);
 
+const map = <T, U>(
+  list: T[],
+  callback: (value: T, index?: number, array?: readonly T[]) => U,
+): U[] => list.map(callback);
+
 const reduce = <T, U>(
   list: T[],
   callback: (previousValue: U, currentValue: T, currentIndex: number, array: T[]) => U,
@@ -39,15 +39,29 @@ const reduceRight = <T, U>(
   initialValue?: U,
 ) => list.reduceRight(callback, initialValue);
 
+const reverse = <T>(list: T[]): T[] => list.reverse();
+
+const slice = <T>(list: T[], start?: number, end?: number) => list.slice(start, end);
+
+const some = <T>(list: T[], predicate: (value: T, index: number, array: T[]) => boolean): boolean =>
+  list.some(predicate);
+
+const sort = <T>(list: T[], compareFn?: ((a: T, b: T) => number) | undefined): T[] =>
+  list.sort(compareFn);
+
 export const E = {
-  map,
-  filter,
   concat,
   every,
+  filter,
   find,
   flat,
   includes,
   join,
+  map,
   reduce,
   reduceRight,
+  reverse,
+  slice,
+  some,
+  sort,
 };
